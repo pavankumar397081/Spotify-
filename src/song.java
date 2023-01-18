fe66ac4c9607d4a2a63094d4756019b51c94ba3c
pavankumar397081 <prajapatipavan2303@gmail.com>
2023-01-18 17:19:48 +0530

    Spotify

---
+++ src/song.java
@@ -0,0 +1,34 @@
+public class song {
+
+    private String title;
+    private double duration;
+
+    public song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+}
